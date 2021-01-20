7aa9c092fea4f4a7dba8332aadb125b7105255dd
DragoonKite <frank.depaolo@yahoo.com>
2021-01-19 21:12:41 -0600

    fixed spacing

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,25 +1,26 @@
 var userIP = "";
 
 var getLocation = function(){
-        $.getJSON("https://api.ipify.org/?format=json", function(e) {
-            var userIP = e.ip;
-            return userIP;
-        }).then(findLocation(userIP));
+    $.getJSON("https://api.ipify.org/?format=json", function(e) {
+        var userIP = e.ip;
+        return userIP;
+    }).then(findLocation(userIP));
 };
 
 var findLocation = function(ip){
     var apiURL = "http://ip-api.com/json/" + ip;
-            fetch(apiURL).then(function(response){
-                if(response.ok){
-                    response.json().then(function(data){
-                        console.log(data)
-                        console.log("Your location is " + data.city + "," + data.region + " " + data.zip + "\n Lat: "  + data.lat + "\n Lon: " + data.lon)
-                    });
-                }
-                else{
-                    //if api fecth fails, alert the user
-                };
+    
+    fetch(apiURL).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data)
+                console.log("Your location is " + data.city + "," + data.region + " " + data.zip + "\n Lat: "  + data.lat + "\n Lon: " + data.lon)
             });
+        }
+        else{
+            //if api fecth fails, alert the user
+        };
+    });
 };
 
 getLocation();
