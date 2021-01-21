4421cbd379d5525cb675063d3207a417d7883d88
DragoonKite <frank.depaolo@yahoo.com>
2021-01-20 21:34:04 -0600

    use nps api to get parks in user state when 'Testing' is clicked

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,21 +1,28 @@
 var userIP = "";
+var userState = "";
+var userLat = 0;
+var userLon = 0;
+
+//NPS api key - URIxdIswkdTl4euYJO1wnZGabiusT8WBy7v5SX5k
+//nps apiURL - https://developer.nps.gov/api/v1
 
 //gets the users ip then passes that data the findLocation function
 var getLocation = function(){
     $.getJSON("https://api.ipify.org/?format=json", function(e) {
         userIP = e.ip;
-    }).then(findLocation(userIP));
+    }).then(findLocation());
 };
 
 //uses ip ddress to get physical location data
-var findLocation = function(ip){
-    var apiURL = "http://ip-api.com/json/" + ip;
+var findLocation = function(){
+    var apiURL = "http://ip-api.com/json/" + userIP;
     
     fetch(apiURL).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data)
-                console.log("Your location is " + data.city + "," + data.region + " " + data.zip + "\n Lat: "  + data.lat + "\n Lon: " + data.lon)
+                userState = data.region;
+                userLat = data.lat;
+                userLon = data.lon;
             });
         }
         else{
@@ -24,5 +31,27 @@ var findLocation = function(ip){
         };
     });
 };
+
+//get state parks in user's state
+var parksInState = function(){
+    console.log(userState);
+    var apiURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + userState + "&api_key=URIxdIswkdTl4euYJO1wnZGabiusT8WBy7v5SX5k";
+    console.log(apiURL)
+
+    fetch(apiURL).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data)
+            });
+        }
+        else{
+            //if api fecth fails, alert the user
+            alert("Error: " + response.statusText)
+        };
+    })
+};
+$("h1").on('click', function(){
+    parksInState();
+})
 
 getLocation();
