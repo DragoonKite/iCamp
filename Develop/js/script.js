087270c9fc6fb47f7eeb259515d4b2214f35f2e4
DragoonKite <frank.depaolo@yahoo.com>
2021-01-24 16:13:19 -0600

    search box data read, checked to see if valid, then state park names displayed and data stored

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,6 +2,10 @@ var userIP = "";
 var userState = "";
 var userLat = 0;
 var userLon = 0;
+var searchedState = "";
+var stateParks = [];
+
+var stateCodes = {AL:'alabama', AK:'alaska', AZ:'arizona', AR:"arkansa", CA:'california', CO:'colorado', CT:'connecticut', DE:'deleware', DC:'district of columbia', FL:'florida', GA:'georgia', HI:'hawaii', ID:'idaho', IL:'illinois',IN:'indiana', IA:'iowa', KS:'kansas', KY:'kentucky', LA:'louisiana', ME:'maine',MD:'maryland', MA:'massachusetts', MI:'michigan', MN:'minnesota', MS:'mississippi',MO:'missouri', MT:'montana', NE:'nebraska', NV:'nevada', NH:'new hampshire', NJ:'new jersey', NM:'new mexico', NY:'new york', NC:'north carolina', ND:'north dakota',OH:'ohio', OK:'oklahoma', OR:'oregon', PA:'pennsylvania', RI:'rhode island', SC:'south carolina', SD:'south dakota', TN:'tennessee', TX:'texas', UT:'utah', VT:'vermont',VA:'virginia', WA:'washington', WV:'west virginia', WI:'wisconsin', WY:'wyoming'};
 
 //NPS api key - URIxdIswkdTl4euYJO1wnZGabiusT8WBy7v5SX5k
 //nps apiURL - https://developer.nps.gov/api/v1
@@ -33,15 +37,27 @@ var findLocation = function(){
 };
 
 //get state parks in user's state
-var parksInState = function(){
-    console.log(userState);
-    var apiURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + userState + "&api_key=URIxdIswkdTl4euYJO1wnZGabiusT8WBy7v5SX5k";
+var parksInState = function(state){
+    var apiURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + state + "&api_key=URIxdIswkdTl4euYJO1wnZGabiusT8WBy7v5SX5k";
     console.log(apiURL)
 
     fetch(apiURL).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data)
+                //makes park list container visable to user
+                $("#parkListContainer").show();
+                //display the name of each national park
+                for(var i=0; i < data.data.length; i++){
+                    var listItem = $("<li>");
+                    listItem.text(data.data[i].fullName);
+                    console.log(listItem)
+                    $("#parkList").append(listItem);
+                };
+
+                //save last searched state data for future use
+                stateParks = []
+                stateParks = data.data;
+                console.log(stateParks);
             });
         }
         else{
@@ -51,8 +67,23 @@ var parksInState = function(){
     })
 };
 
-$("h1").on('click', function(){
-    parksInState();
+$("#stateSearchBtn").on('click', function(){
+    searchedState = $(".submit")[0].value.toLowerCase();
+    //checks to see if the value listed is one fo the states or the short 2 letter state code
+    for(key in stateCodes){
+      
+        if (searchedState === stateCodes[key] || searchedState.toUpperCase() === key){
+            //if so, clear searchbox and send 2 letter code to nps api
+            $(".submit")[0].value = "";
+            parksInState(key)
+            return;
+        }     
+    }
+
+    //if not; clear searchbox and ask user to re-enter a search option
+    $(".submit")[0].value = "";
+    $(".submit")[0].placeholder = "That state is not recognized. Please enter another.";
+
 })
 
 getLocation();
