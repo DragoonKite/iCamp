6fb32f4a94ca3020d3388f27f877aa14d161e123
muncle <blueink38@yahoo.com>
2021-01-31 22:06:33 -0600

    added modal framework

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -123,6 +123,17 @@ var displayWeather = function(){
     });
 };
 
+$(".modal-button").click(function() {
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+ });
+ 
+ $(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+ });
+
 //save up to last three searches
 var saveSearch = function(){
     //remove duplicates
